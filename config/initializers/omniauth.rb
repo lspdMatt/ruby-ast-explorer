8e3e810cd21ab2f82254e76df5fa831b4b1cb4d7
Matthew Giaconelli <matthew.giaconelli@lightspeedhq.com>
2022-02-23 13:38:37 -0500

    make it work

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# config/initializers/omniauth.rb
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],
-           scope: 'user:email,user:follow,gist'
-end
